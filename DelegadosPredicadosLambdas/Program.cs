6e6cb95bd88837529d521bd38a6c35dbf6a6c136
Carlos <cfernande1408@gmail.com>
2022-10-27 10:21:36 +0200

    Agregar archivos de proyecto.

---
+++ DelegadosPredicadosLambdas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DelegadosPredicadosLambdas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //creacion del objeto delegado apuntando a la clase bienvenida y metodo saludo
+            
+            ObjetoDelegado ElDelegado = new ObjetoDelegado(MensajeBienvenida.SaludoBienvenida);
+
+            //utilizacion del delegado para llamar al metodo saludoBiejvenida
+            
+            ElDelegado();
+                
+                
+         }
+
+        //Definición del objeto delegado
+
+        delegate void ObjetoDelegado();
+    }
+
+    class MensajeBienvenida
+    { 
+
+    public static void SaludoBienvenida()
+    {
+            Console.WriteLine("Hola");
+    }
+    }
+
+    class MensajeDespedida
+    {
+
+        public static void SaludoDespedidaa()
+        {
+            Console.WriteLine("Adios");
+        }
+    }
+}
